354fff4fbd2bc8aef643927deae015ffa52f5d38
Christopher Brown <io@henrian.com>
2020-08-14 16:11:23 -0400

    Add IMAGE_SUFFIXES constant

--- photos/__init__.py
+++ photos/__init__.py
@@ -6,3 +6,31 @@ try:
     __version__ = pkg_resources.get_distribution("photos").version
 except Exception:
     pass
+
+IMAGE_SUFFIXES = frozenset(
+    suffix
+    for canonical_suffix in (
+        # typical
+        ".jpg",
+        ".png",
+        ".tif",
+        ".bmp",
+        # raw
+        ".cr2",
+        ".xmp",
+        ".aae",
+        # documents
+        ".pal",
+        ".psp",
+        ".psd",
+        ".pdf",
+        # movies
+        ".thm",
+        ".gif",
+        ".mov",
+        ".mpg",
+        ".mp4",
+        ".avi",
+    )
+    for suffix in (canonical_suffix, canonical_suffix.upper())
+)

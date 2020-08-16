c726811ad9e97980237b6fd73c9c2a63a64ab8e8
Christopher Brown <io@henrian.com>
2020-08-16 11:53:28 -0400

    Use Pil(low) to read exif tags from images

---
+++ photos/metadata.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Collection, Dict
+
+import PIL
+import PIL.ExifTags
+
+EXIF2NAME = PIL.ExifTags.TAGS
+NAME2EXIF = {v: k for k, v in EXIF2NAME.items()}
+
+
+def read_exif_tag(path: Path, name: str) -> str:
+    with PIL.Image.open(path) as image:
+        exif = image.getexif()
+        return exif.get(NAME2EXIF.get(name))
+
+
+def read_exif_tags(path: Path, names: Collection[str] = ()) -> Dict[str, str]:
+    with PIL.Image.open(path) as image:
+        exif = image.getexif()
+        if names:
+            return {name: exif.get(NAME2EXIF.get(name)) for name in names}
+        # return all tags but with remapped names
+        return {EXIF2NAME[exif_id]: value for exif_id, value in exif.items()}

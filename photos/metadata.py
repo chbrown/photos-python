dde3adb67358d0886b43947e7b685f1ec5f3028b
Christopher Brown <io@henrian.com>
2020-08-18 15:31:33 -0400

    Replace Pillow with ExifRead + pyheif to support HEIC images

--- photos/metadata.py
+++ photos/metadata.py
@@ -1,30 +1,84 @@
 from datetime import datetime
 from pathlib import Path
-from typing import Collection, Dict
+from typing import BinaryIO, Dict, Iterator, Optional, Tuple, Union
 import logging
+import io
 
-import PIL
-import PIL.ExifTags
+import exifread
+import pyheif
 
 logger = logging.getLogger(__name__)
 
-EXIF2NAME = PIL.ExifTags.TAGS
-NAME2EXIF = {v: k for k, v in EXIF2NAME.items()}
+TAG2NAME: Dict[int, str] = {
+    tag: name_and_mapper[0] for tag, name_and_mapper in exifread.tags.EXIF_TAGS.items()
+}
+NAME2TAG: Dict[str, int] = {v: k for k, v in TAG2NAME.items()}
 
 
-def read_exif_tag(path: Path, name: str) -> str:
-    with PIL.Image.open(path) as image:
-        exif = image.getexif()
-        return exif.get(NAME2EXIF.get(name))
+def _expand(tag_or_name: Union[int, str]) -> Tuple[int, str]:
+    if isinstance(tag_or_name, int):
+        tag = tag_or_name
+        name = TAG2NAME[tag]
+    else:
+        name = tag_or_name
+        tag = NAME2TAG[name]
+    return tag, name
 
 
-def read_exif_tags(path: Path, names: Collection[str] = ()) -> Dict[str, str]:
-    with PIL.Image.open(path) as image:
-        exif = image.getexif()
-        if names:
-            return {name: exif.get(NAME2EXIF.get(name)) for name in names}
-        # return all tags but with remapped names
-        return {EXIF2NAME[exif_id]: value for exif_id, value in exif.items()}
+def _process_file(
+    fh: BinaryIO,
+    stop_tag: str = exifread.DEFAULT_STOP_TAG,
+    details: bool = True,
+    strict: bool = False,
+    debug: bool = False,
+    truncate_tags: bool = True,
+):
+    """
+    ExifRead claims to handle HEIC (HEIF) images, but it can't handle mine. This is a
+    wrapper that intercepts HEIC images and uses pyheif to extract the exif data, but
+    otherwise hands over directly to ExifRead.
+    """
+    header = fh.read(12)
+    if header[4:12] == b"ftypheic":
+        fh.seek(0)
+        heif_file = pyheif.read(fh)
+        exif_data = next(
+            item["data"] for item in heif_file.metadata if item["type"] == "Exif"
+        )
+        fh = io.BytesIO(exif_data[len(b"Exif\x00\x00") :])
+    return exifread.process_file(
+        fh,
+        stop_tag=stop_tag,
+        details=details,
+        strict=strict,
+        debug=debug,
+        truncate_tags=truncate_tags,
+    )
+
+
+def read_tag_value(path: Path, tag_or_name: Union[int, str]) -> Optional[str]:
+    tag, name = _expand(tag_or_name)
+    with path.open("rb") as fp:
+        hdr_tags = _process_file(fp, stop_tag=name)
+        for ifd_tag in hdr_tags.values():
+            if not isinstance(ifd_tag, bytes) and ifd_tag.tag == tag:
+                return str(ifd_tag)
+    return None
+
+
+def _iter_tags(path: Path, details: bool = True) -> Iterator[Tuple[str, str]]:
+    with path.open("rb") as fp:
+        hdr_tags = _process_file(fp, details=details)
+        for key, ifd_tag in hdr_tags.items():
+            # strip the ifd_name prefix from each key
+            sep_index = key.find(" ")
+            if sep_index != -1:
+                key = key[sep_index + 1 :]
+            yield key, str(ifd_tag)
+
+
+def read_tags(path: Path, details: bool = True) -> Dict[str, str]:
+    return dict(_iter_tags(path, details=details))
 
 
 def parse_exif_datetime(value: str) -> datetime:
@@ -41,9 +95,7 @@ def read_datetime(path: Path) -> datetime:
     `stat` the file and return the birthtime (and if that fails, return the mtime).
     """
     try:
-        return parse_exif_datetime(read_exif_tag(path, "DateTimeOriginal"))
-    except PIL.UnidentifiedImageError as exc:
-        logger.debug("Could not read file as Image (%s)", exc)
+        return parse_exif_datetime(read_tag_value(path, "DateTimeOriginal"))
     except TypeError as exc:
         logger.debug("Could not read Exif data from Image (%s)", exc)
     logger.info("Falling back to filesystem metadata for %s", path)
